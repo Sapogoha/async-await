b4c10ee9c5861da9b9781786c93f44dbde88cedc
Aleksey Sokolov <sokolov.alexey@me.com>
2022-03-09 18:33:50 +0300

    optimised GameSavingLoader

--- src/js/gameSavingLoader.js
+++ src/js/gameSavingLoader.js
@@ -3,10 +3,8 @@ import read from './reader';
 
 export default class GameSavingLoader {
   static async load() {
-    return (async () => {
-      const data = await read();
-      const value = await json(data);
-      return JSON.parse(value);
-    })();
+    const data = await read();
+    const value = await json(data);
+    return JSON.parse(value);
   }
 }
